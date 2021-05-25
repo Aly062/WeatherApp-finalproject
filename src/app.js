3d2a866bfddb60521e870ae6bb7ec18cf44b6126
Aly062 <83830031+Aly062@users.noreply.github.com>
2021-05-25 23:13:28 +0100

    Added relevant icon

--- src/app.js
+++ src/app.js
@@ -27,14 +27,19 @@ function formatDate(timestamp){
   let humidityElement=document.querySelector("#humidity");
   let windElement=document.querySelector("#wind");
   let dateElement=document.querySelector("#date");
+  let iconElement=document.querySelector("#icon");
   temperatureElement.innerHTML= Math.round(response.data.main.temp);
   cityElement.innerHTML= response.data.name;
   descriptionElement.innerHTML= response.data.weather[0].description;
   humidityElement.innerHTML=response.data.main.humidity;
   windElement.innerHTML=Math.round(response.data.wind.speed);
   dateElement.innerHTML=formatDate(response.data.dt* 1000);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
   }
   let apiKey= "a3b7350519e63d3c7b14753ebf179d9b";
-  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  let city= "New York";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
   axios.get(apiUrl).then(displayTemperature);
   
